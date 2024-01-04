24467702655b3ca211dbf6a4a1bd539c8480cdfc
lucashub5 <lucascarovano@gmail.com>
2024-01-04 19:33:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const root = document.documentElement;
+const darkTheme = document.querySelector('.dark-theme');
+const lightTheme = document.querySelector('.light-theme');
+
+const submit = document.getElementById("submit");
+const passwordField = document.getElementById("user_password");
+const confirmPasswordField = document.getElementById("user_confirmpassword");
+
+let themeControl = false;
+
+darkTheme.addEventListener('click', function() {
+    if(!themeControl){
+        themeControl = true;
+        root.className = 'dark';
+    }
+});
+
+lightTheme.addEventListener('click', function() {
+    if(themeControl == true){
+        themeControl = false;
+        root.className = 'light';
+    }
+});
+
+submit.addEventListener('click', function() {
+    const passwordValue = passwordField.value;
+    const confirmPasswordValue = confirmPasswordField.value;
+
+    if (passwordValue  === confirmPasswordValue) {
+    } else {
+        confirmPasswordField.setCustomValidity("Passwords do not match.");
+        confirmPasswordField.reportValidity();
+    }
+});
+
+document.addEventListener('input', function() {
+    confirmPasswordField.setCustomValidity("");
+});
+
